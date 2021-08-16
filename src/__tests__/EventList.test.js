5eea084beb3b3bf7e5778f07575ec964528c5faa
hamedghd <h.gharibdoost@gmail.com>
2021-08-16 17:16:28 +0200

    Add IDs

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -6,7 +6,7 @@ import Event from '../Event';
 // This test will only pass if EventList renders exactly four events from its prop events.
 describe('<EventList /> component', () => {
   test('render correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={[{}, {}, {}, {}]} />);
+    const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
     expect(EventListWrapper.find(Event)).toHaveLength(4);
   });
 });
