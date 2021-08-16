d7fe9f73a351bb37a6721711d2cc07a6f5712f57
hamedghd <h.gharibdoost@gmail.com>
2021-08-16 17:07:30 +0200

    Import Event

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../EventList';
+import Event from '../Event';
 
 // This test will only pass if EventList renders exactly four events from its prop events.
 describe('<EventList /> component', () => {
