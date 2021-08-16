8c1625bdd46a0fc20ce5f09e848ec3c30e256d3b
hamedghd <h.gharibdoost@gmail.com>
2021-08-16 23:12:55 +0200

    Create handleItemClicked function

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -15,6 +15,11 @@ class CitySearch extends Component {
       suggestions,
     });
   };
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion
+    });
+  }
   render() {
     return (
       <div className="CitySearch">
