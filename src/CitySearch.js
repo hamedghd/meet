cd5a88c5ca7c4618e291591bd75ca024348acace
hamedghd <h.gharibdoost@gmail.com>
2021-08-16 22:50:09 +0200

    Display suggestions

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
   state = {
     query: '',
+    suggestions: []
   }
   handleInputChanged = (event) => {
     const value = event.target.value;
@@ -18,6 +19,12 @@ class CitySearch extends Component {
           onChange={this.handleInputChanged}
         />
         <ul className="suggestions">
+          {this.state.suggestions.map((suggestion) => (
+            <li key={suggestion}>{suggestion}</li>
+          ))}
+          <li key='all'>
+            <b>See all cities</b>
+          </li>
         </ul>
       </div>
     );
