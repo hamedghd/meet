de7f379f44c868c90fe112d824764a05e8116c6a
hamedghd <h.gharibdoost@gmail.com>
2021-08-16 17:12:23 +0200

    Render a list of events

--- src/EventList.js
+++ src/EventList.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import Event from './Event';
 class EventList extends Component {
   render() {
+    // Renders a list of events using the events prop:
+    const { events } = this.props;
     return (
       <ul className="EventList">
+        {events.map(event =>
+          <li>
+            <Event event={event} />
+          </li>
+        )}
       </ul>
     );
   }
