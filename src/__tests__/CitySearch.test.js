199a8f628059c32fb73f6844a627de322b543b0e
hamedghd <h.gharibdoost@gmail.com>
2021-08-16 21:00:03 +0200

    Add a new test

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -3,8 +3,14 @@ import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
 
 describe('<CitySearch /> component', () => {
+  // Here, the test checks whether an element with the class name city exists within the CitySearchWrapper component.
   test('render text input', () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
+  });
+  // This test checks for the existence of an element with the class name suggestions in the CitySearchWrapper.
+  test('renders a list of suggestions', () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
 });
