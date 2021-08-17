5958841f7aec6a944a444f61f2fa66754a1dff0c
hamedghd <h.gharibdoost@gmail.com>
2021-08-17 10:45:47 +0200

    Create a test to check the existence of numberOfEvents

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => { }} />);
+  });
+  //
+  test('render text input', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
+  });
+
+});
