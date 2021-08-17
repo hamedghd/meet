5638a9cc9248e6079f8a2912d83f9265aca65043
hamedghd <h.gharibdoost@gmail.com>
2021-08-17 23:55:11 +0200

    Add an integration test to get event as a prop from App

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
@@ -20,6 +20,17 @@ describe('<App /> component', () => {
   //
   test('renders the NumberofEvents Bar', () => {
     expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+  });
+});
+// Integration tests:
+describe('<App /> integration', () => {
+  // This test will be about making sure that EventList gets events as a prop from App, where it will be defined in its state:
+  test('App passes "events" state as a prop to EventList', () => {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state('events');
+    expect(AppEventsState).not.toEqual(undefined);
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
   });
 });
 
