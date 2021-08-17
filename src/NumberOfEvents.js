f2ea145718ae646c50631860157fd9c335cec1c2
hamedghd <h.gharibdoost@gmail.com>
2021-08-17 11:03:56 +0200

    Add a number input for numberOfEvents

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 32,
+  };
   render() {
     return (
-      <div className="numberOfEvents"></div>
+      <div className="numberOfEvents">
+        <input
+          className="event-number-input"
+          type="number"
+          placeholder="Enter Number of Events"
+          id="numberOfEvents__input"
+          value={this.state.numberOfEvents}
+        />
+      </div>
     );
   }
 }
