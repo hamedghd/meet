81c4a4ed0af87334b5507594742a58a7a3408567
hamedghd <h.gharibdoost@gmail.com>
2021-08-17 07:58:52 +0200

    Add a test for checking existance of events

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+
+describe('<Event /> component', () => {
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={mockData[1]} />);
+  });
+  //
+  test('render Event component', () => {
+    expect(EventWrapper.find('.event')).toHaveLength(1);
+  });
+});
