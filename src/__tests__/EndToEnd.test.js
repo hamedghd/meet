9ecf4e68822a968d2791e374be6e4e31d9bae3f6
hamedghd <h.gharibdoost@gmail.com>
2021-08-20 17:47:56 +0200

    Remove the visualized test.

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -11,9 +11,9 @@ describe('show/hide an event details', () => {
   beforeAll(async () => {
     jest.setTimeout(30000);
     browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 250, // slow down by 250ms
-      ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+      //headless: false,
+      //slowMo: 250, // slow down by 250ms
+      //ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
     });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
