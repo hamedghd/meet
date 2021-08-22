ff7620fa9b58127ffa479659fd6ed9dd9996e014
hamedghd <h.gharibdoost@gmail.com>
2021-08-22 16:15:30 +0200

    Add google verification

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import { WarningAlert } from './Alert';
 import './nprogress.css';
+import WelcomeScreen from './WelcomeScreen';
+import { getEvents, extractLocations, checkToken, getAccessToken } from
+  './api';
 
 class App extends Component {
   state = {
@@ -14,9 +17,16 @@ class App extends Component {
     currentLocation: 'all',
     numberOfEvents: 32,
     warningText: '',
+    showWelcomeScreen: undefined,
   }
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+
+
     getEvents().then((events) => {
       if (!navigator.onLine) {
         this.setState({
@@ -55,6 +65,9 @@ class App extends Component {
     });
   }
   render() {
+    // When the showWelcomeScreen state is undefined,
+    // an empty div will be rendered until the state gets either true or false
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
       <div className="App">
         <h1>Meet Application</h1>
@@ -63,6 +76,7 @@ class App extends Component {
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
