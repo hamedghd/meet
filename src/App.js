0bc7c36c074c1f63239d50d4c306d47138eadea5
hamedghd <h.gharibdoost@gmail.com>
2021-08-23 14:42:18 +0200

    Create a function to get the total number of events happening in each city

--- src/App.js
+++ src/App.js
@@ -65,6 +65,17 @@ class App extends Component {
       }
     });
   }
+  //
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
   render() {
     // When the showWelcomeScreen state is undefined,
     // an empty div will be rendered until the state gets either true or false
